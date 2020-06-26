f30ddffd950099db521a3cf27516481c2d3060dd
Phil <p.gibson2003@gmail.com>
2020-06-26 02:07:06 +0100

    Add .gitignore file, remove IntelliJ files and fix a mis-typed password field.

--- app/molecules/form/create-account.php
+++ app/molecules/form/create-account.php
@@ -3,7 +3,7 @@
     <input type="hidden" name="THIS_FORM" value="CREATE_ACCOUNT">
     <input type="text" placeholder="Username" name="username" required>
     <input type="text" placeholder="Email Address" name="email" required>
-    <input type="text" placeholder="Password" name="password" required>
-    <input type="text" placeholder="Confirm Password" name="confirmpassword" required>
+    <input type="password" placeholder="Password" name="password" required>
+    <input type="password" placeholder="Confirm Password" name="confirmpassword" required>
     <input type="submit" value="Create Account">
 </form>
